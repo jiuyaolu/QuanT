4a5b285921c9671aa5b706fd1c9e0782e4bb2d69
jiuyaolu <lujiuyao@gmail.com>
2025-09-11 00:29:09 -0400

    added HIVRC data

---
+++ R/hiv.R
@@ -0,0 +1,17 @@
+#' HIVRC dataset
+#'
+#' HIVRC dataset
+#'
+#' @format
+#' \code{hiv} is a list containing the following components:
+#' \describe{
+#'   \item{otu}{the absolute abundance(count) matrix}
+#'   \item{rela}{the relative abundance matrix}
+#'   \item{groupid}{the group(study) indicator}
+#'   \item{hivstatus}{a factor with two levels}
+#'   \item{age}{a vector with continuous values}
+#'   \item{bmi}{a vector with continuous values}
+#'   \item{gender}{a factor with two levels}
+#' }
+"hiv"
+
